f7472c9df99f3999fde5d5b6c859ca928e6c99ec
qgdd <897350469@qq.com>
2025-04-23 20:03:56 +1200

    update

--- src/Model_switch.py
+++ src/Model_switch.py
@@ -83,32 +83,25 @@ def evaluate(num_workers, dataset_name,Sampling,Sampling_Numbers,modellist,resul
 
     log_out(dataset_name, results,llm)
 def generate(dataset_name,question,llm,results_sampling):
-    prompt_head="Question:\nJames gets 10 new CDs.  Each CD cost $15.  He gets them for 40% off.  He decides he doesn't like 5 of them and sells them for 40. How much money was he out? \n\nAnswer:\nFirst, let us rewrite the question with labels.\n#1. James gets 10 new CDs. \n#2. Each CD cost $15, and he gets them for 40% off.\n#3. He sells 5 of them for 40.\n#4. How much money was he out?\nNext, let's answer the question step by step with reference to the question and reasoning process. Your final answer should be in the form \"The answer to this question is \\boxed{answer}\", at the end of your response:\n\n#5. (by #2) Step 1: Calculate the price of each CD after the 40% discount.\nOriginal price per CD: $15, Discount: 40%\nPrice per CD after discount: $15 * (1 - 0.40) = $15 * 0.60 = $9\n\n#6. (by#1 #5) Step 2: Calculate the total cost of the 10 CDs. \nPrice per CD after discount: $9, Total CDs: 10\nTotal cost of 10 CDs: $9 * 10 = $90\n\n#7. (by #3) Step 3: Calculate the total money he gets back from selling 5 CDs. \nMoney from selling 5 CDs: $40\n\n#8. (by #6 #7) Step 4: Calculate the total amount of money James is out. \nTotal cost of 10 CDs: $90\nMoney from selling 5 CDs: $40\nMoney James is out: $90 - $40 = $50\n\n#9. (by #4 #8) The original question is #4. How much money was he out? We do not miss information on the rewritten labels. So the answer to this question is John is out $50. The answer is  \\boxed{answer}."
-    prompt_tail="\n\nAnswer:\nFirst, let us rewrite the question with labels.\n\n"
-    prompt=prompt_head+f"Question:\n{question}\n"+prompt_tail
+    prompt_tail="\n\nThink step by step and slove this question. Your final answer should be in the form \"The answer to this question is \\boxed{X}\", X is your answer and at the end of your response:\n\n"
+    prompt=f"Question:\n{question}\n"+prompt_tail
     outputs=reasoning_layer(input=prompt,n_Sampling=results_sampling,llm=llm)
     inputs=outputs
-    if dataset_name in ["gsm8k","MGSM","mathbench"]:
+    if dataset_name in ["GSM8K","MGSM","MathBench"]:
         outputs=extract_final_answers_gsm8k(inputs)
         filtered_outputs = [item for item in outputs if item != "Error"]
         most_frequent_ans=most_frequent(filtered_outputs)
         output=most_frequent_ans
-    elif dataset_name in ["math","AGIEval_math","math500"]:
+    elif dataset_name in ["MATH"]:
         outputs=get_boxed_answer(inputs)
         most_frequent_ans=most_frequent(outputs)
         output=most_frequent_ans
-    elif dataset_name in ["mmlu","mmlu_pro","AGIEval","csqa","truthfulqa","logiqa"]:
+    elif dataset_name in ["MMLU-Pro"]:
         outputs=get_boxed_answer(inputs)
         outputs=get_last_uppercase(outputs)
         most_frequent_ans=most_frequent(outputs)
         output=most_frequent_ans
-    elif dataset_name=="last_letters":
-        outputs=get_boxed_answer(inputs)
-        outputs=[item.replace(" ","").lower() for item in outputs]
-        filtered_outputs = [item for item in outputs if item != "Error" and item != ""]
-        most_frequent_ans=most_frequent(filtered_outputs)
-        output=most_frequent_ans
-    elif dataset_name=="date":
+    elif dataset_name=="DATE":
         outputs=get_boxed_answer(inputs)
         most_frequent_ans=most_frequent(outputs)
         output=most_frequent_ans
